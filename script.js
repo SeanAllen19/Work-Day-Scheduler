07917eef230b6df2b2f2a948f1ad8e531e53da21
SeanAllen19 <saallen19.dev@gmail.com>
2023-01-10 18:23:43 -0600

    Added date and some local store code

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveButton = $('.saveBtn');
+var inputBox = $('.description');
+var timeOfDay = $('.time-block');
+
+// inputBox.on('click', function () {
+//   console.log('Hello World');
+// });
+
+function saveComment() {
+  var input = {
+    comment: inputBox.val().trim(),
+  };
+  console.log(input)
+ 
+  localStorage.setItem("input", JSON.stringify(input));
+}
+
+saveButton.on('click', function (event) {
+  event.preventDefault();
+  console.log(timeOfDay)
+  saveComment();
+});
+
+
+
+//// If the refomatDate is < reformateDate then it should be grey, if the reformatDate is > than the reformatDate it should be green. Current hour should be red ( look to getting a date var with 24hrs in DayJS?)
+// if (reformatDate < reformatDate) {
+//   console.log(passed)
+// } else ≈
+var time = dayjs().format('hh:mm:ss');
+if (time < time) {
+  console.log('true');
+} else if (time > time) {
+  console.log('over')
+}
+function getDate() {
+var reformatDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
+$('#currentDay').text(reformatDate);
+};
+setInterval(getDate, 1000)
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // useful when saving the description in local storage? 
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -21,3 +64,7 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+
+
